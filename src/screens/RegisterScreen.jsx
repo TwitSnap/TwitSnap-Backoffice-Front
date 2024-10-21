c8c9ac21e5ffca23496d0592a464716d096e205f
ibertolini <56204692+nacho-1@users.noreply.github.com>
2024-10-21 19:01:04 -0300

    feat: Agrego campo para el PIN en el registro

--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -4,6 +4,7 @@ import register from "../handlers/register.js";
 
 export default function RegisterScreen() {
     const [email, setEmail] = useState('');
+    const [pin, setPin] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
@@ -11,7 +12,7 @@ export default function RegisterScreen() {
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        if (!email || !password || !confirmPassword) {
+        if (!email || !pin || !password || !confirmPassword) {
             setError('All fields are required.');
             return;
         }
@@ -22,9 +23,9 @@ export default function RegisterScreen() {
         }
 
         setError('');
-        console.log('Registering with', {email, password});
+        console.log('Registering with', {email, password, pin});
         try {
-            await register(email, password);
+            await register(email, password, pin);
         } catch (error) {
             setError(error.message);
         }
@@ -40,6 +41,15 @@ export default function RegisterScreen() {
                         type="email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        style={styles.input}
+                        required
+                    />
+                </div>
+                <div style={styles.inputContainer}>
+                    <label>PIN</label>
+                    <input
+                        value={pin}
+                        onChange={(e) => setPin(e.target.value)}
                         style={styles.input}
                         required
                     />
